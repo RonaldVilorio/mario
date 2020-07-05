f2edaf45db9b297b1d740562cba0d0aea1d7f736
RonaldVilorio <ronvilorio@gmail.com>
2020-07-04 21:54:30 -0400

    can stand/fall on 3 green blocks

--- script.js
+++ script.js
@@ -12,67 +12,50 @@ let mario = {
   width: 20,
   height: 20,
   coordX: 10,
-  coordY: 280,
-  gravity: -0.5,
+  coordY: 260,
+  gravity: -1.0,
 };
 mario.img = new Image();
 mario.img.src = "./assets/mario.png";
 
-let floorBlock = {
-  coordX: 0,
-  coordY: 300,
-  width: 100,
-  height: canvas.height,
-  id: 0,
-};
-floorBlock.img = new Image();
-floorBlock.img.src = "./assets/floorBlock2.png";
-
-let block = {
-  coordX: 100,
-  coordY: 240,
-  width: 100,
-  height: 60,
-  danger: false,
-};
-
-// let breakableBlock = {
-//   coordX: 300,
-//   coordY: 240,
-//   width: 20,
+// let floorBlock = {
+//   coordX: 0,
+//   coordY: 300,
+//   width: 100,
 //   height: 20,
-//   danger: false,
 // };
+
 let minion = {
   coordX: 10,
   coordY: 280,
   width: 20,
   height: 20,
-  gravity: -0.5,
+  gravity: -9.5,
 };
 
 document.addEventListener("keydown", keyDownHandler);
 document.addEventListener("keyup", keyUpHandler);
 
 let blocks = [
-  createBlock(100, 240, 100, 60, "green"),
-  createBlock(320, 240, 20, 20, "darkblue"),
-  createBlock(340, 240, 20, 20, "darkblue"),
+  createBlock(100, 270, 50, 50, "green", false, false),
+  createBlock(200, 270, 50, 50, "green", false, false),
+  createBlock(300, 270, 50, 50, "green", false, false),
+  createBlock(0, 300, 400, 20, "brown", false, true),
 ];
 
-function createBlock(x, y, w, h, c) {
+function createBlock(x, y, w, h, c, d, f) {
   return {
     coordX: x,
     coordY: y,
     width: w,
     height: h,
-    danger: false,
+    danger: d,
     color: c,
+    floor: f,
   };
 }
 function drawMario() {
   ctx.beginPath();
-
   ctx.drawImage(
     mario.img,
     mario.coordX,
@@ -95,26 +78,6 @@ function drawBlocks() {
     ctx.fill();
     ctx.closePath();
   }
-}
-
-function drawFloorBlock() {
-  ctx.beginPath();
-  //   ctx.drawImage(
-  //     floorBlock.img,
-  //     floorBlock.coordX,
-  //     floorBlock.coordY,
-  //     floorBlock.width,
-  //     floorBlock.height
-  //   );
-  ctx.rect(
-    floorBlock.coordX,
-    floorBlock.coordY,
-    floorBlock.width,
-    floorBlock.height
-  );
-  ctx.fillStyle = "brown";
-  ctx.fill();
-  ctx.closePath();
 }
 function keyDownHandler(e) {
   if (e.key === "Right" || e.key === "ArrowRight") {
@@ -144,8 +107,6 @@ function keyUpHandler(e) {
   }
 }
 function move() {
-  // handles the green block interaction
-
   if (rightPressed && upPressed) {
     mario.coordY -= 50;
     mario.coordX += 20;
@@ -157,56 +118,74 @@ function move() {
   } else if (leftPressed) {
     mario.coordX -= 10;
   } else if (upPressed) {
-    mario.coordY -= 55;
+    mario.coordY -= 50;
   } else if (downPressed) {
-    null;
+    mario.coordY += 5;
   } else {
     null;
   }
 }
-function dontFall() {
-  for (let i = 0; i < blocks.length; i++) {
-    console.log("M", mario.coordX + mario.width, "B", blocks[i].coordX);
-    // standing on the blocks
-    // falling from the sides
-    if (
-      (mario.coordX + mario.width > blocks[i].coordX + blocks[i].width &&
-        mario.coordY + mario.height === blocks[i].coordY) ||
-      (blocks[i].coordX == mario.coordX + mario.width &&
-        mario.coordY + mario.height === blocks[i].coordY)
-    ) {
-      mario.coordY -= mario.gravity;
-    } else if (mario.coordY + mario.height === blocks[i].coordY) {
-      null;
-    } else if (mario.coordY < floorBlock.coordY - mario.height) {
-      mario.coordY -= mario.gravity;
-    } 
-    // mario falls into empty space
-    if (mario.coordX >= 500 && mario.coordX <= 580) {
-      mario.coordY -= mario.gravity;
-    }
 
-    // if (
-    //   mario.coordY == blocks[i].coordY - mario.height &&
-    //   mario.coordX + mario.width >= blocks[i].coordX &&
-    //   mario.coordX <= blocks[i].width + blocks[i].coordX
-    // ) {
-    //   console.log("hey");
-    //   null;
-    // } else if (mario.coordY < floorBlock.coordY - mario.height) {
-    //   mario.coordY -= mario.gravity;
-    // } else if (mario.coordX >= 500 && mario.coordX <= 580) {
-    //   mario.coordY -= mario.gravity;
-    // }
+// let blocks = [
+//   createBlock(320, 240, 20, 20, "darkblue", false, false),
+//   createBlock(340, 240, 20, 20, "darkblue", false, false),
+//   createBlock(100, 270, 50, 50, "green", false, false),
+//   createBlock(200, 270, 50, 50, "green", false, false),
+//   createBlock(0, 300, 400, 20, "brown", false, true),
+// ];
+function handleBlockStand(blks) {
+  let greenBlocks = blks.filter((el) => el.color === "green");
+  let floor = blks.filter((el) => el.floor === true);
+  let gblock1 = greenBlocks[0];
+  let gblock2 = greenBlocks[1];
+
+  if (floor[0].coordY === mario.coordY + mario.height) {
+    mario.gravity = 0;
+  } else if (
+    mario.coordY + mario.height === gblock1.coordY &&
+    (mario.coordX + mario.width <= gblock1.coordX ||
+      mario.coordX >= gblock1.coordX + gblock1.width)
+  ) {
+    console.log("dedede");
+    mario.gravity = -1.0;
+  } else if (mario.coordY + mario.height === gblock1.coordY) {
+    console.log("hey");
+    mario.gravity = 0;
+  } else {
+    mario.gravity = -1.0;
+  }
+}
+function dontFall() {
+  // handleBlockStand(blocks);
+
+  for (let i = 0; i < blocks.length; i++) {
+    if (blocks[i].floor) {
+      if (blocks[i].coordY === mario.coordY + mario.height) {
+        mario.gravity = 0;
+      }
+    } else if (blocks[i].color === "green") {
+      if (
+        mario.coordX + mario.width <= blocks[i].coordX ||
+        mario.coordX >= blocks[i].coordX + blocks[i].width
+      ) {
+        console.log("dedede");
+        mario.gravity = -1.0;
+      } else if (mario.coordY + mario.height === blocks[i].coordY) {
+        console.log("hey");
+        mario.gravity = 0;
+        break;
+      } else {
+        mario.gravity = -1.0;
+      }
+    }
 
     // jumping up to erase blue blocks
-    if (
-      mario.coordX === blocks[i].coordX &&
-      blocks[i].coordY - mario.coordY == -20
-    ) {
-      blocks.splice(blocks.indexOf(blocks[i]), 1);
-    }
-
+    // if (
+    //   mario.coordX === blocks[i].coordX &&
+    //   blocks[i].coordY - mario.coordY == -20
+    // ) {
+    //   blocks.splice(blocks.indexOf(blocks[i]), 1);
+    // }
     // green block phasing stoppage
     if (blocks[i].coordX - mario.coordX == 10 && blocks[i].height > 20) {
       mario.coordX = mario.coordX - 10;
@@ -220,27 +199,11 @@ function dontFall() {
   }
 }
 function drawAll() {
-  // clears everything above the floor
-  ctx.clearRect(0, 0, canvas.width, canvas.height - 100);
-  // clears mario when falling
-  ctx.clearRect(500, 200, 100, canvas.height);
+  // clears everything
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawMario();
   drawBlocks();
+  mario.coordY -= mario.gravity;
   dontFall();
-  // mario falls and gets reset
-  if (mario.coordY > 400) {
-    mario.coordX = 10;
-    mario.coordY = 270;
-  }
 }
-
-function displayFloorBs() {
-  for (let i = 0; i < 8; i++) {
-    if (floorBlock.coordX != 500) {
-      drawFloorBlock();
-    }
-    floorBlock.coordX = floorBlock.coordX + 100;
-  }
-}
-displayFloorBs();
 setInterval(drawAll, 10);
